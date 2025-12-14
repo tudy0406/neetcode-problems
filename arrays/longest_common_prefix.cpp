38cb8f354808544cd79652c5c4ebf9ec89e511bd
Tudy0406 <radtudor7@gmail.com>
2025-12-14 20:54:22 +0200

    longet_common_prefix

---
+++ arrays/longest_common_prefix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res = "";
+        for(int i = 0; i<strs[0].length(); i++){
+            for(int j = 1; j<strs.size(); j++){
+                if(i >= strs[j].length() || strs[0][i] != strs[j][i]){
+                    return res;
+                }
+            }
+            res += strs[0][i];
+        }  
+        return res;
+    }
+};

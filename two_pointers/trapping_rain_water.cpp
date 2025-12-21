08986315c1e3682272ca1bd6661609b9b740f3c1
Tudy0406 <radtudor7@gmail.com>
2025-12-21 21:10:13 +0200

    trapping_rainwater

---
+++ two_pointers/trapping_rain_water.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        vector<int> preffix(height.size());
+        vector<int> suffix(height.size());
+
+        preffix[0] = height[0];
+        for(int i = 1; i<height.size(); ++i)
+            preffix[i] = max(preffix[i-1], height[i]);
+
+        suffix[height.size()-1] = height[height.size()-1];
+        for(int i = height.size()-2; i>=0; --i)
+            suffix[i] = max(suffix[i+1], height[i]);
+
+        int totalWater = 0;
+        for(int i = 0; i<height.size(); ++i)
+            totalWater += min(preffix[i], suffix[i]) - height[i];
+
+        return totalWater;
+    }
+};

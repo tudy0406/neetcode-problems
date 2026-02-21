3592a21f102b0466f6c60a1afc47784d5027a4b0
Tudy0406 <radtudor7@gmail.com>
2026-02-21 15:42:53 +0200

    subtree of another tree

--- trees/subtree_of_another_tree.py
+++ trees/subtree_of_another_tree.py
@@ -5,15 +5,27 @@
 #         self.left = left
 #         self.right = right
 
-class Solution:
+class Solution:   
 
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if (p and not q) or (not p and q):
+    def isSameTree(self, p : Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if (not p and q) or (p and not q):
             return False
-        if not p:
+        if not p and not q:
             return True
         if p.val != q.val:
             return False
         
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        self.isSame = False
+        def traversal(p1: Optional[TreeNode], q1: Optional[TreeNode]) -> None:
+            if not p1:
+                return None
+            traversal(p1.left, q1)
+            traversal(p1.right, q1)
+            if self.isSameTree(p1, q1):
+                self.isSame = True
+        traversal(root, subRoot)
+        return self.isSame
         

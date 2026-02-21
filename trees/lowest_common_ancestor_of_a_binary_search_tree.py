884488a67d92911c05468a49fa438a2140c74221
Tudy0406 <radtudor7@gmail.com>
2026-02-21 15:41:59 +0200

    lowest ancestor

--- trees/lowest_common_ancestor_of_a_binary_search_tree.py
+++ trees/lowest_common_ancestor_of_a_binary_search_tree.py
@@ -5,27 +5,11 @@
 #         self.left = left
 #         self.right = right
 
-class Solution:   
-
-    def isSameTree(self, p : Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if (not p and q) or (p and not q):
-            return False
-        if not p and not q:
-            return True
-        if p.val != q.val:
-            return False
-        
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        self.isSame = False
-        def traversal(p1: Optional[TreeNode], q1: Optional[TreeNode]) -> None:
-            if not p1:
-                return None
-            traversal(p1.left, q1)
-            traversal(p1.right, q1)
-            if self.isSameTree(p1, q1):
-                self.isSame = True
-        traversal(root, subRoot)
-        return self.isSame
-        
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if (root.val >= p.val and root.val <= q.val) or (root.val <= p.val and root.val >= q.val):
+            return root
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return self.lowestCommonAncestor(root.right, p, q)
